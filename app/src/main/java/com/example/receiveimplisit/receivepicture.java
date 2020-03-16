1bfda4b31642b92c18bb008ad67ee3e255d8f565
eby8zevin <36786100+eby8zevin@users.noreply.github.com>
2020-03-16 15:14:50 +0700

    Receive Only

---
+++ app/src/main/java/com/example/receiveimplisit/receivepicture.java
@@ -0,0 +1,15 @@
+package com.example.receiveimplisit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class receivepicture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receivepicture);
+        setTitle("Receive Picture");
+    }
+}
