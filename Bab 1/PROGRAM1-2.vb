ff5e67be9b8c06b1f151d8472213df2b4cd05a46
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-08-12 07:06:27 +0700

    Create PROGRAM1-2.vb

---
+++ Bab 1/PROGRAM1-2.vb
@@ -0,0 +1,20 @@
+'********************************************************
+'Nama file	: DemoKomentar.vb
+'Deskripsi	: Program ini akan mendemonstrasikan penggunaan 
+'                 komentar di dalam Visual Basic
+'Dibuat         : 1/1/2016
+'*********************************************************
+
+'Menggunakan namespace System
+Imports System
+
+'Membuat modul
+Module Program
+  'Membuat metode (prosedur) utama
+  Sub Main()
+    'Menampilkan bilangan 0 sampai 9 ke layar
+    For i As Integer = 0 To 9
+      Console.WriteLine(i)
+    Next
+  End Sub
+End Module
