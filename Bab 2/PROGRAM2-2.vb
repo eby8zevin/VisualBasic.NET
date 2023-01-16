20d875c11795aea2c7b7a8eb2af151eb8cbbba98
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-01-17 05:01:55 +0700

    Create PROGRAM2-2.vb

---
+++ Bab 2/PROGRAM2-2.vb
@@ -0,0 +1,23 @@
+Module Program
+  Sub Main()
+    'mendeklarasikan variabel
+    Dim panjang As Integer
+    Dim lebar As Integer    
+    
+    'mengisi nilai ke dalam variabel
+    panjang = 7
+    lebar = 5
+    
+    'melakukan perhitungan dan menampung
+    'hasilnya ke dalam variabel luas
+    Dim luas As Integer = panjang * lebar
+    
+    'menampilkan luas
+    Console.WriteLine("PERSEGI PANJANG")
+    Console.WriteLine("panjang {0} = {1}", Chr(9), panjang)
+    Console.WriteLine("lebar {0} = {1}", Chr(9), lebar)
+    Console.WriteLine("luas persegi panjang {0} = {1}", Chr(9), luas)
+    
+    Console.ReadLine()
+  End Sub
+End Module
