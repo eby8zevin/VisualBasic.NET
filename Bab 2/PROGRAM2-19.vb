2888c80a6b131f71b17a55e4e08075661b8aea8d
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-01-22 05:39:49 +0700

    Create PROGRAM2-19.vb

---
+++ Bab 2/PROGRAM2-19.vb
@@ -0,0 +1,14 @@
+Module Program
+  Sub Main()        
+    'mendeklarasikan variabel bertipe Object
+    Dim produk, path As Object
+    
+    produk = "Visual Basic"
+    path = "C:\Program Files\Microsoft Visual Studio 11.0"
+    
+    Console.WriteLine(produk)
+    Console.WriteLine(path)    
+    
+    Console.ReadLine()
+  End Sub
+End Module
