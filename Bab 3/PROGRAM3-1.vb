df74d10e1371fbb18c51c9c4a0e0fa70c3cdf809
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-01-22 05:41:47 +0700

    Create PROGRAM3-1.vb

---
+++ Bab 3/PROGRAM3-1.vb
@@ -0,0 +1,40 @@
+Module Program  
+  Sub Main()            
+    Console.WriteLine("10 + 3 {0}= {1}", _
+      Chr(9), (10 + 3))
+    Console.WriteLine("10 - 3 {0}= {1}", _
+      Chr(9), (10 - 3))
+    Console.WriteLine("10 * 3 {0}= {1}", _
+      Chr(9), (10 * 3))
+    
+    Console.WriteLine()
+    
+    Console.WriteLine("10 / 3 {0}= {1}", _
+      Chr(9), (10 / 3))
+    Console.WriteLine("10.0 / 3.0 {0}= {1}", _
+      Chr(9), (10.0 / 3.0))
+    
+    Console.WriteLine()
+    
+    Console.WriteLine("10 \ 3 {0}= {1}", _
+      Chr(9), (10 \ 3))
+    Console.WriteLine("10.0 \ 3.0 {0}= {1}", _
+      Chr(9), (10.0 \ 3.0))
+    
+    Console.WriteLine()
+    
+    Console.WriteLine("10 Mod 3 {0}= {1}", _
+      Chr(9), (10 Mod 3))
+    Console.WriteLine("10.0 Mod 3.0 {0}= {1}", _
+      Chr(9), (10.0 Mod 3.0))
+    
+    Console.WriteLine()
+    
+    Console.WriteLine("2 ^ 5 {0}= {1}", _
+      Chr(9), (2 ^ 5))
+    Console.WriteLine("5 ^ 2 {0}= {1}", _
+      Chr(9), (5 ^ 2))
+    
+    Console.ReadLine()
+  End Sub
+End Module
