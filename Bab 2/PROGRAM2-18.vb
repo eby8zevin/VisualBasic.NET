5449ec0bf4688df334f19b61ac2641a9abad7d8d
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-01-21 13:17:33 +0700

    Create PROGRAM2-18.vb

---
+++ Bab 2/PROGRAM2-18.vb
@@ -0,0 +1,24 @@
+Module Program
+  Sub Main()        
+    'mendeklarasikan variabel bertipe Object
+    Dim ob As Object
+        
+    'memasukkan nilai Integer ke ob
+    ob = 100
+    Console.WriteLine("ob: {0}", ob)
+    
+    'memasukkan nilai Double ke ob
+    ob = 12.75
+    Console.WriteLine("ob: {0}", ob)
+    
+    'memasukkan nilai Boolean ke ob
+    ob = True
+    Console.WriteLine("ob: {0}", ob)
+    
+    'memasukkan nilai String ke ob
+    ob = "Visual Basic .NET"
+    Console.WriteLine("ob: {0}", ob)    
+    
+    Console.ReadLine()
+  End Sub
+End Module
