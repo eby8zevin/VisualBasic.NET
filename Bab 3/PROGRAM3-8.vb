d66418d9e7cee9ca39d37ea636cb3df01f07a51f
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-01-23 13:31:25 +0700

    Create PROGRAM3-8.vb

---
+++ Bab 3/PROGRAM3-8.vb
@@ -0,0 +1,12 @@
+Module Program  
+  Sub Main()            
+    Dim x As Integer = 12
+    
+    Console.WriteLine("12 >> 1 {0}= {1}", Chr(9), x >> 1)
+    Console.WriteLine("12 >> 2 {0}= {1}", Chr(9), x >> 2)
+    Console.WriteLine("12 << 1 {0}= {1}", Chr(9), x << 1)
+    Console.WriteLine("12 << 2 {0}= {1}", Chr(9), x << 2)
+        
+    Console.ReadLine()
+  End Sub
+End Module
