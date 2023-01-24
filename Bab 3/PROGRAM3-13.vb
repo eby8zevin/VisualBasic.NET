1c5cb0b47d0baa3f8f10bbce800eb9b144ee7e0b
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-01-24 16:45:50 +0700

    Create PROGRAM3-13.vb

---
+++ Bab 3/PROGRAM3-13.vb
@@ -0,0 +1,11 @@
+Module Program  
+  Sub Main()            
+    Dim s As String = "Microsoft"
+        
+    If (TypeOf(s) Is System.String) Then 
+      Console.WriteLine("s bertipe String")
+    End If
+    
+    Console.ReadLine()
+  End Sub
+End Module
