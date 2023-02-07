c9134596c9060ad1fa01231ded1470e45e4c5b5d
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-02-08 05:19:51 +0700

    Create PROGRAM8-16.vb

---
+++ Bab 8/PROGRAM8-16.vb
@@ -0,0 +1,41 @@
+Class KelasDasar
+  Protected bilangan As Integer
+  
+  Public Sub New(ByVal i As Integer)
+    bilangan = i
+  End Sub  
+End Class
+
+Class KelasTurunan: Inherits KelasDasar
+  'akan menimpa/menyembunyikan data KelasDasar.bilangan
+  'tanpa menimbulkan pesan peringatan
+  'pada saat kompilasi kode program
+  Shadows Private bilangan As Integer
+  
+  Public Sub New(ByVal i1 As Integer, _
+                 ByVal i2 As Integer)
+    MyBase.New(i1)
+    'akan mengisi nilai ke KelasTurunan.bilangan
+    bilangan = i2
+  End Sub
+  
+  Public Sub CetakData()
+    Console.WriteLine("KelasDasar.bilangan {0}: {1}", _
+      vbTab, MyBase.bilangan)
+    Console.WriteLine("KelasTurunan.bilangan {0}: {1}", _
+      vbTab, bilangan) 'bisa ditulis: Me.bilangan
+  End Sub
+  
+End Class
+
+Module Program
+  Sub Main()
+    'membuat objek dari kelas KelasTurunan
+    Dim obj As KelasTurunan = New KelasTurunan(100, 200)
+    
+    'memanggil metode CetakData()
+    obj.CetakData()
+    
+    Console.ReadLine()
+  End Sub
+End Module
