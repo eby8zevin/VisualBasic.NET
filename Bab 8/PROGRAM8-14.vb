7b6de83be88a63092901f6f86973ab2b11b55edd
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-02-08 05:19:13 +0700

    Create PROGRAM8-14.vb

---
+++ Bab 8/PROGRAM8-14.vb
@@ -0,0 +1,28 @@
+Class KelasDasar
+  
+  Public Overridable Sub Metode1()
+    Console.WriteLine("KelasDasar.Metode1()")
+  End Sub
+  
+  Public Sub Metode2()
+    'memanggil metode Metode1() menggunakan Me
+    Me.Metode1()
+  End Sub
+  
+End Class
+
+Class KelasTurunan: Inherits KelasDasar
+  'melakukan override terhadap metode Metode1()
+  Public Overrides Sub Metode1()
+    Console.WriteLine("KelasTurunan.Metode1()")
+  End Sub
+End Class
+
+Module Program
+  Sub Main()
+    Dim obj As KelasTurunan = New KelasTurunan()
+    obj.Metode2()
+
+    Console.ReadLine()
+  End Sub
+End Module
