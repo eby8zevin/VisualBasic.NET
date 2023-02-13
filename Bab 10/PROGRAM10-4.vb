34a572712bca22878a3566785b3b30b644feff5c
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-02-14 03:56:21 +0700

    Create PROGRAM10-4.vb

---
+++ Bab 10/PROGRAM10-4.vb
@@ -0,0 +1,56 @@
+Class BilanganBulat
+  Private __nilai As Integer
+  
+  'konstruktor
+  Public Sub New(ByVal nilai As Integer)
+    __nilai = nilai
+  End Sub
+  
+  'properti Nilai
+  Public Property Nilai() As Integer
+    Set
+      __nilai = Value
+    End Set
+    Get
+      Return __nilai
+    End Get
+  End Property
+  
+  'overload operator *
+  Public Shared Operator *(ByVal bil1 As BilanganBulat, _
+                           ByVal bil2 As BilanganBulat) _
+                           As BilanganBulat
+    Return New BilanganBulat(bil1.__nilai * bil2.__nilai)    
+  End Operator
+  
+  'overload operator \
+  Public Shared Operator \(ByVal bil1 As BilanganBulat, _
+                           ByVal bil2 As BilanganBulat) _
+                           As BilanganBulat
+    Return New BilanganBulat(bil1.__nilai \ bil2.__nilai)    
+  End Operator
+  
+End Class
+
+Module Program
+  Sub Main()
+    Dim a As BilanganBulat = New BilanganBulat(15)
+    Dim b As BilanganBulat = New BilanganBulat(5)
+    
+    'operator kali (*) dan bagi (\) dapat diterapkan 
+    'untuk tipe BilanganBulat
+    Dim c As BilanganBulat = a * b
+    Dim d As BilanganBulat = a \ b
+        
+    Console.WriteLine("a.Nilai {0}: {1}", _
+      vbTab, a.Nilai)
+    Console.WriteLine("b.Nilai {0}: {1}", _
+      vbTab, b.Nilai)
+    Console.WriteLine("c.Nilai {0}: {1}", _
+      vbTab, c.Nilai)
+    Console.WriteLine("d.Nilai {0}: {1}", _
+      vbTab, d.Nilai)
+    
+    Console.ReadLine()
+  End Sub
+End Module
