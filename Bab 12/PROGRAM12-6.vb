d73c6197f8663b67334a5b946d68bca74a8522be
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-02-16 18:59:31 +0700

    Create PROGRAM12-6.vb

---
+++ Bab 12/PROGRAM12-6.vb
@@ -0,0 +1,13 @@
+Module Program  
+  Sub Main()    
+    Dim s As String = "VB.NET"
+    
+    For Each c As Char In s
+      'menampilkan karakter 
+      'yang disimpan di dalam variabel c
+      Console.WriteLine(c)
+    Next
+            
+    Console.ReadLine()
+  End Sub
+End Module
