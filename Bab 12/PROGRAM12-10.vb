5c0c36dc808e37b90df56fbc35dfb671d4d67f42
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-02-18 08:43:00 +0700

    Create PROGRAM12-10.vb

---
+++ Bab 12/PROGRAM12-10.vb
@@ -0,0 +1,13 @@
+Module Program  
+  Sub Main()    
+    
+    Dim s1 As String = "Microsoft"
+    Dim s2 As String = String.Copy(s1)
+    
+    'menampilkan string
+    Console.WriteLine("s1 : ""{0}""", s1)
+    Console.WriteLine("s2 : ""{0}""", s2)   
+    
+    Console.ReadLine()
+  End Sub
+End Module
