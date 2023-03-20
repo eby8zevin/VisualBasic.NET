de1acdfb09020525d5c1fd66214f05f23f3d1d67
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-03-20 16:37:17 +0700

    Create PROGRAM18-11.vb

---
+++ Bab 18/PROGRAM18-11.vb
@@ -0,0 +1,32 @@
+Imports System.Collections.Generic
+
+Module Program
+  Sub Main()
+    'membuat koleksi bertipe List(Of T)
+    Dim list As List(Of String) = New List(Of String)()
+    list.Add("Java")
+    list.Add("Visual Basic .NET")
+    list.Add("Ruby")
+    list.Add("Python")    
+    list.Add("C#")
+    
+    'menampilkan isi koleksi
+    'sebelum diurutkan
+    Console.WriteLine("Isi list sebelum pengurutan:")
+    For Each elemen As String In list
+      Console.WriteLine(elemen)
+    Next
+    
+    'mengurutkan elemen
+    list.Sort()
+        
+    'menampilkan isi koleksi
+    Console.WriteLine()
+    Console.WriteLine("Isi list setelah pengurutan:")
+    For Each elemen As String In list
+      Console.WriteLine(elemen)
+    Next       
+    
+    Console.ReadLine()
+  End Sub
+End Module
