56bc42383c64f58b4e8d7a4a492b17d3b36ae474
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-04-04 18:38:44 +0700

    Create PROGRAM19-28.vb

---
+++ Bab 19/PROGRAM19-28.vb
@@ -0,0 +1,96 @@
+Imports System.Linq
+Imports System.Collections.Generic
+
+Class Pelanggan
+  'properti
+  Public Property NamaDepan As String
+  Public Property NamaBelakang As String
+  Public Property NoTelepon As String
+  Public Property Kota As String
+  
+  'konstruktor
+  Public Sub New(ByVal nd As String, _
+                 ByVal nb As String, _
+                 ByVal nt As String, _
+                 ByVal k As String)
+    NamaDepan = nd
+    NamaBelakang = nb
+    NoTelepon = nt
+    Kota = k
+  End Sub  
+End Class
+
+Module Program  
+  Sub Main()
+    'sumber data berupa List(Of T)
+    Dim list As List(Of Pelanggan) = _
+      New List(Of Pelanggan)()
+    
+    'menambah data ke dalam list
+    list.Add( _
+      New Pelanggan("Adi", "Wardhana", _
+                    "022-25166555", "Bandung"))
+    list.Add( _
+      New Pelanggan("Kartika", "Dewi", _
+                    "022-72011222", "Bandung"))
+    list.Add( _
+      New Pelanggan("Akhmad", "Maulana", _
+                    "022-20099888", "Bandung"))
+    list.Add( _
+      New Pelanggan("Cindy", "Melani", _
+                    "021-80822111", "Jakarta"))
+    list.Add( _
+      New Pelanggan("Dewi", "Ayuandira", _
+                    "022-25177555", "Bandung"))
+    list.Add( _
+      New Pelanggan("Akhmad", "Kosyim", _
+                    "021-77700111", "Jakarta"))
+    list.Add( _
+      New Pelanggan("Alex", "Hendrawan", _
+                    "021-77711222", "Jakarta"))
+    list.Add( _
+      New Pelanggan("Bambang", "Wiryo", _
+                    "024-83144555", "Semarang"))
+    list.Add( _
+      New Pelanggan("Joko", "Santoso", _
+                    "024-83144999", "Semarang"))
+    
+    'membuat query
+    Dim hasil As IEnumerable(Of Pelanggan) = _
+      list.Where(Function(p) p.Kota = "Semarang") _
+          .Select(Function(e) e)
+    
+    'menampilkan elemen-elemen dalam sumber data
+    Console.WriteLine("Sumber data:{0}", vbNewLine)
+    Console.WriteLine("{0}{1}{2}{3}{4}{5}{6}", _
+      "NAMA", vbTab, vbTab, vbTab, "NO TELEPON", _
+      vbTab, "KOTA")
+    For Each p As Pelanggan In list
+      Console.WriteLine("{0} {1}{2}{3}{4}{5}{6}",
+                        p.NamaDepan, _
+                        p.NamaBelakang, _
+                        vbTab, vbTab, _
+                        p.NoTelepon, _
+                        vbTab, _
+                        p.Kota)
+    Next
+    
+    'menampilkan elemen-elemen hasil query
+    Console.WriteLine()
+    Console.WriteLine("Hasil query:{0}", vbNewLine)
+    Console.WriteLine("{0}{1}{2}{3}{4}{5}{6}", _
+      "NAMA", vbTab, vbTab, vbTab, "NO TELEPON", _
+      vbTab, "KOTA")
+    For Each p As Pelanggan In hasil
+      Console.WriteLine("{0} {1}{2}{3}{4}{5}{6}",
+                        p.NamaDepan, _
+                        p.NamaBelakang, _
+                        vbTab, vbTab, _
+                        p.NoTelepon, _
+                        vbTab, _
+                        p.Kota)
+    Next
+    
+    Console.ReadLine()
+  End Sub
+End Module
