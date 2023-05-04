258edb12e6c28aa78d4ce57ca052b505bd86b820
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-05-05 05:44:29 +0700

    Create Form1.vb

---
+++ Bab 21/DemoCheckbox/DemoCheckbox/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+  Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+    Dim hobi As String = "Hobi:"
+
+    If cbxOlahraga.Checked Then
+      hobi += vbNewLine + cbxOlahraga.Text
+    End If
+    If cbxMembaca.Checked Then
+      hobi += vbNewLine + cbxMembaca.Text
+    End If
+    If cbxTamasya.Checked Then
+      hobi += vbNewLine + cbxTamasya.Text
+    End If
+    If cbxLain.Checked Then
+      hobi += vbNewLine + cbxLain.Text
+    End If
+
+    MessageBox.Show(hobi)
+  End Sub
+End Class
