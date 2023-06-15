531ad35b416bbc5b0e08a6d807be3108bc7a546e
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-06-15 09:36:16 +0700

    Create MainForm.vb

---
+++ Bab 21/DemoForm/DemoForm/MainForm.vb
@@ -0,0 +1,6 @@
+﻿Public Class MainForm
+
+    Private Sub btnHello_Click(sender As Object, e As EventArgs) Handles btnHello.Click
+        txtHello.Text = "Hello World!"      'kode yang Anda tulis
+    End Sub
+End Class
