66dea2cdcc1d79e2225cb3214ab536f7566c07ef
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-07-19 10:32:20 +0700

    Create MainForm.vb

---
+++ Bab 21/DemoMaskTextBox/DemoMaskTextBox/MainForm.vb
@@ -0,0 +1,29 @@
+﻿Public Class MainForm
+
+  Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    mtbNIP.Mask = "LLL-000"
+    mtbNama.Mask = "?????????????????????????"
+    mtbTglLahir.Mask = "00/00/0000"
+    mtbJenisKelamin.Mask = "&"
+    mtbTinggiBadan.Mask = "000"
+    mtbNoTelepon.Mask = "(9999) 000-0000"
+  End Sub
+
+  Private Sub btnOK_Click(sender As Object, e As EventArgs) Handles btnOK.Click
+    Dim s As String = ""
+    s &= String.Format("NIP{0}{1}: ", vbTab, vbTab) _
+      & mtbNIP.Text
+    s &= String.Format("{0}Nama{1}{2}: ", vbNewLine, vbTab, vbTab) _
+      & mtbNama.Text
+    s &= String.Format("{0}Tgl. Lahir{1}{2}: ", vbNewLine, vbTab, vbTab) _
+      & mtbTglLahir.Text
+    s &= String.Format("{0}Jenis Kelamin{1}: ", vbNewLine, vbTab) _
+      & mtbJenisKelamin.Text
+    s &= String.Format("{0}Tinggi Badan{1}: ", vbNewLine, vbTab) _
+      & mtbTinggiBadan.Text
+    s &= String.Format("{0}No. Telepon{1}: ", vbNewLine, vbTab) _
+      & mtbNoTelepon.Text
+    MessageBox.Show(s, "Informasi")
+  End Sub
+
+End Class
