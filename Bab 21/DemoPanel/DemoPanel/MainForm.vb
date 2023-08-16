09dea715b0876441271d81deba83795f054ae954
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-08-16 08:26:10 +0700

    Create MainForm.vb

---
+++ Bab 21/DemoPanel/DemoPanel/MainForm.vb
@@ -0,0 +1,17 @@
+﻿Public Class MainForm
+
+  Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    WebBrowser1.Navigate(txtURL.Text)
+  End Sub
+
+  Private Sub WebBrowser1_ProgressChanged(sender As Object, e As WebBrowserProgressChangedEventArgs) Handles WebBrowser1.ProgressChanged
+    Application.DoEvents()
+    ProgressBar1.Maximum = Convert.ToInt32(e.MaximumProgress)
+    If e.CurrentProgress > 0 And _
+        e.CurrentProgress < e.MaximumProgress Then
+      ProgressBar1.Value = Convert.ToInt32(e.CurrentProgress)
+    Else
+      ProgressBar1.Value = ProgressBar1.Minimum
+    End If
+  End Sub
+End Class
