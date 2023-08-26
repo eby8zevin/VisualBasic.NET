9e1d67bfc08b8383f32805eb4c43bd8700b3f773
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-08-27 03:48:20 +0700

    Create Form1.vb

---
+++ Bab 21/DemoPictureBox/DemoPictureBox/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+  Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    PictureBox1.ImageLocation = "D:\images\Penguins.jpg"
+    PictureBox1.Load()
+  End Sub
+
+  Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+    If CheckBox1.Checked Then
+      PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+    Else
+      PictureBox1.SizeMode = PictureBoxSizeMode.Normal
+    End If
+  End Sub
+End Class
