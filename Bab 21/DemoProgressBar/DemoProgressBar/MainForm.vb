e77d98fa48b8bbacbdda6fe5e680b225fcd5791d
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-09-05 21:15:32 +0700

    Create MainForm.vb

---
+++ Bab 21/DemoProgressBar/DemoProgressBar/MainForm.vb
@@ -0,0 +1,24 @@
+﻿Public Class MainForm
+
+  Private Sub btnMulai_Click(sender As Object, e As EventArgs) Handles btnMulai.Click
+    Dim max As Integer = 100000
+    Progress.Minimum = 0
+    Progress.Maximum = max
+    Progress.Step = 1
+
+    lbGanjil.Items.Clear()
+    lbGenap.Items.Clear()
+
+    For i As Integer = 1 To max
+      If (i Mod 2) <> 0 Then
+        lbGanjil.Items.Add(i.ToString())
+      Else
+        lbGenap.Items.Add(i.ToString())
+      End If
+      Progress.Value = i
+      'menjadikan dua proses dapat dijalankan 
+      'secara simultan
+      Application.DoEvents()
+    Next
+  End Sub
+End Class
