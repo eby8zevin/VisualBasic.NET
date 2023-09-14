583f40fd14dd04cdf1ff6acdf07cb97b680371c0
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-09-15 02:03:04 +0700

    Create MainForm.vb

---
+++ Bab 21/DemoRadioButton/DemoRadioButton/MainForm.vb
@@ -0,0 +1,21 @@
+﻿Public Class MainForm
+  Private warnaStandar As System.Drawing.Color
+
+  Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    warnaStandar = Me.BackColor
+  End Sub
+
+  Private Sub rb_CheckedChanged(sender As Object, e As EventArgs) Handles rbStandar.CheckedChanged, rbMerah.CheckedChanged, rbHijau.CheckedChanged, rbBiru.CheckedChanged
+    Dim rbAktif As RadioButton = CType(sender, RadioButton)
+    If rbAktif Is rbStandar Then
+      Me.BackColor = warnaStandar
+    ElseIf rbAktif Is rbMerah Then
+      Me.BackColor = System.Drawing.Color.Red
+    ElseIf rbAktif Is rbHijau Then
+      Me.BackColor = System.Drawing.Color.Green
+    Else
+      Me.BackColor = System.Drawing.Color.Blue
+    End If
+  End Sub
+
+End Class
