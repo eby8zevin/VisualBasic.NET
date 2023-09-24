6d1ec6b5e64b082c6543d83ffcdc26082232fffd
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-09-25 04:34:26 +0700

    Create MainForm.vb

---
+++ Bab 21/DemoTabControl/DemoTabControl/MainForm.vb
@@ -0,0 +1,44 @@
+﻿Public Class MainForm
+
+  Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    btnBack.Enabled = False
+    'tabControl1.SelectedTab = tabPage1
+    TabControl1.SelectedIndex = 0
+  End Sub
+
+  Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+    If TabControl1.SelectedIndex > 0 Then
+      If TabControl1.SelectedIndex = 1 Then
+        btnBack.Enabled = False
+      End If
+      TabControl1.SelectedIndex -= 1
+      If Not btnNext.Enabled Then btnNext.Enabled = True
+    End If
+  End Sub
+
+  Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+    If TabControl1.SelectedIndex < _
+       TabControl1.TabPages.Count Then
+      If TabControl1.SelectedIndex = _
+         TabControl1.TabPages.Count - 2 Then
+        btnNext.Enabled = False
+      End If
+      TabControl1.SelectedIndex += 1
+      If Not btnBack.Enabled Then btnBack.Enabled = True
+    End If
+  End Sub
+
+  Private Sub TabControl1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles TabControl1.SelectedIndexChanged
+    Dim index As Integer = TabControl1.SelectedIndex
+    If index = 0 Then
+      btnBack.Enabled = False
+      btnNext.Enabled = True
+    ElseIf index = TabControl1.TabPages.Count - 1 Then
+      btnBack.Enabled = True
+      btnNext.Enabled = False
+    Else
+      btnBack.Enabled = True
+      btnNext.Enabled = True
+    End If
+  End Sub
+End Class
