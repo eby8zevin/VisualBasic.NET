afefdbf7ed435f44750da85c4f893b4da294cc1c
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-10-09 10:46:43 +0700

    Create MainForm.vb

---
+++ Bab 21/DemoWebBrowser/DemoWebBrowser/MainForm.vb
@@ -0,0 +1,29 @@
+﻿Public Class MainForm
+
+  Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Me.AcceptButton = btnGo
+  End Sub
+
+  Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+    WebBrowser1.GoBack()
+  End Sub
+
+  Private Sub btnForward_Click(sender As Object, e As EventArgs) Handles btnForward.Click
+    WebBrowser1.GoForward()
+  End Sub
+
+  Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+    Dim address As String = txtURL.Text
+    'Dim URL As System.Uri = New System.Uri(address)
+    'WebBrowser1.Navigate(URL)
+    WebBrowser1.Navigate(address)
+  End Sub
+
+  Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+    WebBrowser1.Stop()
+  End Sub
+
+  Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+    WebBrowser1.Refresh()
+  End Sub
+End Class
